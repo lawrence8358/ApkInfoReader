b2ae54a68a947220ef74ac25ec55c96808fb136e
Lawrence Shen <lawrence8358@hotmail.com>
2020-03-19 08:52:32 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ApkInfoReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button_Select = new System.Windows.Forms.Button();
+            this.textBox_Path = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_Info = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(11, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(716, 60);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "設定 Apk 位置";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.tableLayoutPanel1.Controls.Add(this.button_Select, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_Path, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 24);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(710, 32);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button_Select
+            // 
+            this.button_Select.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_Select.Location = new System.Drawing.Point(601, 4);
+            this.button_Select.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Select.Name = "button_Select";
+            this.button_Select.Size = new System.Drawing.Size(106, 24);
+            this.button_Select.TabIndex = 1;
+            this.button_Select.Text = "選擇 Apk";
+            this.button_Select.UseVisualStyleBackColor = true;
+            this.button_Select.Click += new System.EventHandler(this.button_Select_Click);
+            // 
+            // textBox_Path
+            // 
+            this.textBox_Path.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Path.Location = new System.Drawing.Point(3, 4);
+            this.textBox_Path.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_Path.Name = "textBox_Path";
+            this.textBox_Path.Size = new System.Drawing.Size(592, 27);
+            this.textBox_Path.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_Info);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(11, 73);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(716, 374);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Apk 資訊";
+            // 
+            // textBox_Info
+            // 
+            this.textBox_Info.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Info.Location = new System.Drawing.Point(3, 24);
+            this.textBox_Info.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_Info.Multiline = true;
+            this.textBox_Info.Name = "textBox_Info";
+            this.textBox_Info.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Info.Size = new System.Drawing.Size(710, 346);
+            this.textBox_Info.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 460);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(11, 13, 11, 13);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "APK Info Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_Select;
+        private System.Windows.Forms.TextBox textBox_Path;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox_Info;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApkInfoReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string apkPath = Properties.Settings.Default.ApkPath;
+
+            if (!string.IsNullOrEmpty(apkPath) && File.Exists(apkPath))  
+            {
+                textBox_Path.Text = apkPath;
+                openFileDialog1.InitialDirectory = Path.GetDirectoryName(apkPath);
+                textBox_Info.Text = ReadInfo(apkPath);
+            }
+            else
+            {
+                openFileDialog1.InitialDirectory = Application.StartupPath;
+            } 
+        }
+
+        private void button_Select_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "apk files (*.apk)|*.apk";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                openFileDialog1.InitialDirectory = Path.GetDirectoryName(fileName);
+                textBox_Path.Text = fileName;
+                Properties.Settings.Default.ApkPath = fileName;
+                Properties.Settings.Default.Save();
+
+                textBox_Info.Text = ReadInfo(fileName); 
+            }
+        }
+         
+        private string ReadInfo(string apkPath)
+        {
+            if (string.IsNullOrEmpty(apkPath) || !File.Exists(apkPath)) return string.Empty;
+
+            ICSharpCode.SharpZipLib.Zip.ZipInputStream zip = new ICSharpCode.SharpZipLib.Zip.ZipInputStream(File.OpenRead(apkPath));
+            var filestream = new FileStream(apkPath, FileMode.Open, FileAccess.Read);
+            ICSharpCode.SharpZipLib.Zip.ZipFile zipfile = new ICSharpCode.SharpZipLib.Zip.ZipFile(filestream);
+            ICSharpCode.SharpZipLib.Zip.ZipEntry item;
+
+            string content = string.Empty;
+
+            while ((item = zip.GetNextEntry()) != null)
+            {
+                if (item.Name == "AndroidManifest.xml")
+                {
+                    byte[] bytes = new byte[50 * 1024];
+
+                    Stream strm = zipfile.GetInputStream(item);
+                    int size = strm.Read(bytes, 0, bytes.Length);
+
+                    using (BinaryReader s = new BinaryReader(strm))
+                    {
+                        byte[] bytes2 = new byte[size];
+                        Array.Copy(bytes, bytes2, size);
+                        AndroidDecompress decompress = new AndroidDecompress();
+                        content = decompress.decompressXML(bytes);
+                        break;
+                    }
+                }
+            }
+
+            return content;
+        }
+    }
+}
